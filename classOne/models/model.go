fdcb35a1e21976378a9a991535a31c6c6f4e545e
MyName <my@example.com>
2019-08-14 06:57:44 +0800

    beego新闻发布系统1.0

---
+++ classOne/models/model.go
@@ -0,0 +1,56 @@
+package models
+
+import (
+	"time"
+
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+//表的设计
+
+//定义一个结构体
+type User struct {
+	Id       int
+	Name     string
+	PassWord string
+	//Pass_Word
+	Articles []*Article `orm:"reverse(many)"`
+}
+
+type Article struct {
+	Id       int       `orm:"pk;auto"`
+	ArtiName string    `orm:"size(20)"`
+	Atime    time.Time `orm:"auto_now"`
+	Acount   int       `orm:"default(0);null"`
+	Acontent string    `orm:"size(500)"`
+	Aimg     string    `orm:"size(100)"`
+
+	ArticleType *ArticleType `orm:"rel(fk);on_delete(set_null);null"`
+	Users       []*User      `orm:"rel(m2m)"`
+}
+
+//类型表
+type ArticleType struct {
+	Id       int
+	TypeName string `orm:"size(20)"`
+
+	Articles []*Article `orm:"reverse(many)"`
+}
+
+//在ORM里面__是有特殊含义的
+
+func init() {
+	//ORM操作数据库
+	//获取连接对象
+	orm.RegisterDataBase("default", "mysql", "root:123456@tcp(192.168.152.121:3306)/newsweb?charset=utf8")
+
+	//创建表
+	orm.RegisterModel(new(User), new(Article), new(ArticleType))
+
+	//生成表
+	//第一个参数是数据库别名，第二个参数是是否强制更新
+	orm.RunSyncdb("default", false, true)
+	//操作表
+
+}
